41887cacfd69635f3d15625158d52648b6be5579
user <user@ubuntu.myguest.virtualbox.org>
2023-10-11 18:39:07 +0300

    add alias generation

---
+++ internal/lib/random/random.go
@@ -0,0 +1,18 @@
+package random
+
+import (
+	"math"
+	"math/rand"
+	"time"
+)
+
+const englishAlphabet = "abcdefjhigklmnopqrstuvwxyz"
+
+func NewRandomString(length int) string {
+	randomString := ""
+	for i := 0, i < length, i++ {
+		rand.Seed(time.Now().UnixNano())
+		randomString += englishAlphabet[rand.Intn(len(englishAlphabet))]
+	}
+	return randomString
+}
